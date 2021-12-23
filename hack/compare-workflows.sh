d9452c3c21bf914e9872106fc3fb815c9aea3a01
Jesse Suen <jessesuen@users.noreply.github.com>
2021-12-23 11:03:20 -0800

    chore: update argo-cd to v2.2.1 (#5)
    Signed-off-by: Jesse Suen <jessesuen@gmail.com>

--- hack/compare-workflows.sh
+++ hack/compare-workflows.sh
@@ -2,15 +2,18 @@
 # Script to compare against upstream version for differences
 
 PROJECT_ROOT=$(cd $(dirname ${BASH_SOURCE})/..; pwd)
-upstream_version=master
+chart_root="${PROJECT_ROOT}/charts/argo-workflows"
+upstream_version=v$(grep appVersion ${chart_root}/Chart.yaml | awk '{print $2}')
 
 mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 
 helm template \
     --include-crds \
-    --set global.image.repository=quay.io/argoproj/argocd \
-    --set global.image.tag=latest \
-    --namespace argocd $PROJECT_ROOT/charts/argo-workflows | grep -v imagePullPolicy > $mytmpdir/helm.yaml
+    --set controller.image.repository=quay.io/argoproj/workflow-controller \
+    --set executor.image.repository=quay.io/argoproj/argoexec \
+    --set server.image.repository=quay.io/argoproj/argocli \
+    --set global.image.tag=${upstream_version} \
+    --namespace argo ${chart_root} | grep -v imagePullPolicy > $mytmpdir/helm.yaml
 
 echo """
 apiVersion: kustomize.config.k8s.io/v1beta1
@@ -22,7 +25,7 @@ resources:
 helm_out=$(kustomize build $mytmpdir)
 
 upstream_out=$(
-    curl --silent https://raw.githubusercontent.com/argoproj/argo-workflows/${upstream_version}/manifests/install.yaml | \
+    curl -L --silent https://github.com/argoproj/argo-workflows/releases/download/${upstream_version}/install.yaml | \
     grep -v imagePullPolicy | \
     grep -v "This is an auto-generated file"
 )
