06ad6df3a2d9713567e28204079689ba7bb56fea
Jesse Suen <jesse@akuity.io>
2022-03-30 16:49:55 -0700

    feat: upgrade to workflows v3.3.1
    Signed-off-by: Jesse Suen <jesse@akuity.io>

--- hack/compare-workflows.sh
+++ hack/compare-workflows.sh
@@ -5,29 +5,44 @@ PROJECT_ROOT=$(cd $(dirname ${BASH_SOURCE})/..; pwd)
 chart_root="${PROJECT_ROOT}/charts/argo-workflows"
 upstream_version=v$(grep appVersion ${chart_root}/Chart.yaml | awk '{print $2}')
 
-mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
-
+helm_tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'helm')
+helm dependency update ${chart_root} 2>&1 >/dev/null
 helm template \
     --include-crds \
     --set controller.image.repository=quay.io/argoproj/workflow-controller \
     --set executor.image.repository=quay.io/argoproj/argoexec \
     --set server.image.repository=quay.io/argoproj/argocli \
     --set global.image.tag=${upstream_version} \
-    --namespace argo ${chart_root} | grep -v imagePullPolicy > $mytmpdir/helm.yaml
+    --namespace argo ${chart_root} | grep -v imagePullPolicy > $helm_tmpdir/helm.yaml
 
 echo """
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
+
+namespace: argo
 resources:
 - helm.yaml
-""" > $mytmpdir/kustomization.yaml
+""" > $helm_tmpdir/kustomization.yaml
 
-helm_out=$(kustomize build $mytmpdir)
 
-upstream_out=$(
-    curl -L --silent https://github.com/argoproj/argo-workflows/releases/download/${upstream_version}/install.yaml | \
-    grep -v imagePullPolicy | \
-    grep -v "This is an auto-generated file"
-)
+upstream_tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'upstream')
+echo """
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
 
-diff <(echo "$helm_out") <(echo "$upstream_out")
+namespace: argo
+resources:
+- https://github.com/argoproj/argo-workflows/releases/download/${upstream_version}/install.yaml
+""" > $upstream_tmpdir/kustomization.yaml
+
+
+diff_dir=$(mktemp -d 2>/dev/null || mktemp -d -t 'diff')
+helm_out=$(kustomize build $helm_tmpdir > $diff_dir/helm.yaml)
+upstream_out=$(kustomize build $upstream_tmpdir \
+    | grep -v imagePullPolicy \
+    | grep -v "^data: null$" \
+    > $diff_dir/upstream.yaml)
+diff $diff_dir/upstream.yaml $diff_dir/helm.yaml
+
+echo "Helm template output is located in: $diff_dir/helm.yaml"
+echo "Upstream output is located in: $diff_dir/upstream.yaml"
